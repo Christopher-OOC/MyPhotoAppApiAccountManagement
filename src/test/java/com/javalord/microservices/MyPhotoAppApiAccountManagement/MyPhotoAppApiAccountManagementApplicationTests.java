7da827142434026a312a885087176c133efe566a
Christopher-OOC <olojedechristopher24@gmail.com>
2025-03-15 08:54:43 +0100

    coomit

---
+++ src/test/java/com/javalord/microservices/MyPhotoAppApiAccountManagement/MyPhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalord.microservices.MyPhotoAppApiAccountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
